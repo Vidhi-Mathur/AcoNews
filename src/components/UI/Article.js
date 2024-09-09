dd82c00625d0dfc250d666d4e69234fc138adfe4
Vidhi <mathurvidhi2505@gmail.com>
2024-09-09 19:23:45 +0530

    Fixed unclickable url

--- src/components/UI/Article.js
+++ src/components/UI/Article.js
@@ -2,8 +2,8 @@ import EastIcon from '@mui/icons-material/East';
 
 export const Article = ({ title, description, image, url }) => {
     return (
-        <div className="bg-[#002b4d] rounded-lg shadow border border-[#83fdff]">
-            <img src={image || "/placeholder.svg"} alt={title} width={400} height={225} className="rounded-t-lg object-cover w-full aspect-video"/>
+        <div className="bg-[#002b4d] rounded-lg shadow border border-[#83fdff] cursor-pointer hover:shadow-[0_0_15px_5px_rgba(131,253,255,0.2)] transition-shadow duration-300 ease-in-out hover:scale-105">
+            <img src={image} alt={title} width={400} height={225} className="rounded-t-lg object-cover w-full aspect-video"/>
             <div className="p-4">
                 <h3 className="text-lg font-bold mb-2">{title}</h3>
                 <p className="text-[#99ccff] mb-4">{description}</p>
